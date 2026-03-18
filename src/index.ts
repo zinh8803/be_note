47a92783cab3aedc972bf55777009fe8446cfbae
zinh8803 <ngoquocvinh2003@gmail.com>
2026-03-18 16:04:14 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(cors({ origin: process.env.URL_CLIENT, credentials: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use('/api', apiRoutes)
+app.use('/', apiRoutes)
 app.use(errorHandler)
 
 app.listen(PORT, () => {
